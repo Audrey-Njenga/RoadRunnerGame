284cab5e1cb18afcfc5aca38943296e3ee288b28
Audrey-Njenga <a.njenga@alustudent.com>
2020-06-23 20:37:35 +0300

    Add game views

---
+++ script/gameStart.js
@@ -0,0 +1,39 @@
+function fillGrid(){
+
+}
+
+
+
+function tilePicker() {
+    var tile;
+    switch (number) {
+        case 0:
+            tile = "";
+        case 1:
+            tile = "/assets/images/boulder.jpg";
+            break;
+        case 2:
+            tile = "/assets/images/pothole.jpg";
+            break;
+        case 3:
+            tile = "/assets/images/explosive.jpg";
+            break;
+        case 4:
+            tile = "/assets/images/coyote.jpg";
+            break;
+        case 5:
+            tile = "/assets/images/tarred.jpg";
+            break;
+        case 6:
+            tile = "/assets/images/gold.jpg";
+            break;
+        case 8:
+            tile = "/assets/images/start.jpg";
+            break;
+        case 9:
+            tile = "/assets/images/goal.jpg";
+            break;
+
+    }
+    return tile;
+}
