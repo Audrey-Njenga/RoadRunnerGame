2462b9c33c7fbbd9c9da096aaaf28e6ba051006d
ahmedmeshref <a.meshref@alustudent.com>
2020-07-05 21:05:20 +0200

    group all text_files under input_files

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-file = open("./static/script/testinput.txt", "r")
-file_input = file.readlines()
-grid = []
-for x in file_input:
-    for s in x:
-        if s == "\n" or s == " ":
-            continue
-        else:
-            grid.append(int(s))
-gameRow = grid[0]
-gameCol = grid[1]
 
 @app.route("/")
 def home():
     return render_template("index.html", gameGrid=grid, gameRow=gameRow, gameCol=gameCol)
+
+def read_file(filename):
+    file = open(f"./static/assets/input_files/{filename}", "r")
+    file_input = file.readlines()
+    grid = []
+    for row in file_input:
+        for s in x:
+            if s == "\n" or s == " ":
+                continue
+            else:
+                grid.append(int(s))
+    gameRow = grid[0]
+    gameCol = grid[1]
 
 
 if __name__ == "__main__":
