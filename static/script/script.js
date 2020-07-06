937eb0674141c7b9601518eea82ae457bd7f3032
ahmedmeshref <a.meshref@alustudent.com>
2020-07-06 21:46:20 +0200

    Arrow movement works

--- static/script/script.js
+++ static/script/script.js
@@ -1,27 +1,48 @@
-
 const keys = {
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40
 };
 
-function handleKey (e) {
+function handleKey(e) {
+    let start_item = document.querySelector(".grid-item.selected"),
+        row = start_item.getAttribute("data-row"),
+        col = start_item.getAttribute("data-column"),
+        arrow = false;
     switch (e.keyCode) {
         case keys.left:
+            col = Number.parseInt(col) - 1;
             alert('Left key is pressed');
+            arrow = true;
             break;
         case keys.up:
-             alert('Up  key is pressed')
-             break;
+            row = Number.parseInt(row) - 1;
+            alert('Up  key is pressed');
+            arrow = true;
+            break;
         case keys.right:
-            alert('Right key is pressed')
-             break;
+            col = Number.parseInt(col) + 1;
+            alert('Right key is pressed');
+            arrow = true;
+            break;
         case keys.down:
-            alert('Down key is pressed')
+            row = Number.parseInt(row) + 1;
+            alert('Down key is pressed');
+            arrow = true;
             break;
     }
-};
-
+    if (arrow) {
+        let target_item = document.querySelector(`.grid-item[data-row='${row}'][data-column='${col}']`);
+        if (target_item) {
+            target_item.classList.add("selected");
+            let target_img = target_item.querySelector('img');
+            target_img.src = '/static/assets/images/start.jpg';
+            start_item.classList.remove("selected");
+            start_item.classList.add("deactivated");
+            console.log("moved successfully");
+        }
+    }
+}
 
 window.addEventListener("keydown", handleKey);
