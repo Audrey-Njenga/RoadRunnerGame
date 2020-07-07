04e23fcf6bb4a0abab96b3d2b4147f1ec2b0bdc9
ahmedmeshref <a.meshref@alustudent.com>
2020-07-07 18:17:08 +0200

    Implement move method for the player

--- static/script/script.js
+++ static/script/script.js
@@ -1,3 +1,7 @@
+const game = {
+    score: document.getElementById("score")
+};
+
 const keys = {
     left: 37,
     up: 38,
@@ -13,36 +17,44 @@ function handleKey(e) {
     switch (e.keyCode) {
         case keys.left:
             col = Number.parseInt(col) - 1;
-            alert('Left key is pressed');
             arrow = true;
             break;
         case keys.up:
             row = Number.parseInt(row) - 1;
-            alert('Up  key is pressed');
             arrow = true;
             break;
         case keys.right:
             col = Number.parseInt(col) + 1;
-            alert('Right key is pressed');
             arrow = true;
             break;
         case keys.down:
             row = Number.parseInt(row) + 1;
-            alert('Down key is pressed');
             arrow = true;
             break;
     }
     if (arrow) {
-        let target_item = document.querySelector(`.grid-item[data-row='${row}'][data-column='${col}']`);
-        if (target_item) {
-            target_item.classList.add("selected");
-            let target_img = target_item.querySelector('img');
-            target_img.src = '/static/assets/images/start.jpg';
-            start_item.classList.remove("selected");
-            start_item.classList.add("deactivated");
-            console.log("moved successfully");
-        }
+        return move(start_item, row, col);
     }
 }
+
+let addClass = (target_item, clsName) => {
+    target_item.classList.add(clsName);
+}
+
+let removeClass = (target_item, clsName) => {
+    target_item.classList.add(clsName);
+}
+
+let move = (start_item, row, col) => {
+    let target_item = document.querySelector(`.grid-item[data-row='${row}'][data-column='${col}']`);
+    // Validate movement
+    if (target_item) {
+        addClass(target_item, "Selected")
+        removeClass(start_item, "selected");
+        addClass(start_item, "deactivated");
+        console.log("moved successfully");
+    }
+}
+
 
 window.addEventListener("keydown", handleKey);
